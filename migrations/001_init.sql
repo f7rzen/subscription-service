ab3b76d3c8fa7e2bfb52033038b8a039025e5b57
Artem Kudinov <akudinov04@mail.ru>
2026-06-28 18:09:39 +0300

    add postgres init migration

---
+++ migrations/001_init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS subscriptions (
+    id UUID PRIMARY KEY,
+    service_name TEXT NOT NULL,
+    price INTEGER NOT NULL CHECK (price > 0),
+    user_id UUID NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+    CHECK (end_date IS NULL OR end_date >= start_date)
+);
+
+CREATE INDEX IF NOT EXISTS idx_subscriptions_user_service
+    ON subscriptions(user_id, service_name);
